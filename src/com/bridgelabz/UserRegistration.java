c7498c7b0cab9b95acdd4d4671e56c03bc11de00
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-30 10:03:29 +0530

    Added files

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -5,19 +5,35 @@ import java.util.Scanner;
 
 public class UserRegistration {
 	
-	static void checkStringFormat() {
-		System.out.println("Enter the first Name");
+	static void checkFirstAndLastName() {
+		String nameCondition = "[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(nameCondition);
+		
 		Scanner sc = new Scanner(System.in);
-		String fName = sc.nextLine();
-		String fNameCheck = "[A-Z]{1}[a-z]{2,}";
-		 
-		Pattern pattern = Pattern.compile(fNameCheck);
-		Matcher matcher = pattern.matcher(fName);
-		System.out.println(matcher.matches());
+		System.out.println("Enter the first name");
+		String fName = sc.nextLine();//Scanning first name
+		Matcher fmatcher = pattern.matcher(fName);
+		if(fmatcher.matches()) {
+			System.out.println("Valid Input");
+		}
+		else {
+			System.out.println("Invalid Input");
+		}
+		
+		System.out.println("Enter the Last name");
+		String LName = sc.nextLine();//Scanning Last name 
+		Matcher lastName = pattern.matcher(LName);
+		if(lastName.matches()) {
+			System.out.println("Valid Input");
+		}
+		else {
+			System.out.println("Invalid Input");
+		}
+		
 		sc.close();
 	}
 	
 	public static void main(String[] args) {
-		checkStringFormat();
+		checkFirstAndLastName();
 	}
 }
