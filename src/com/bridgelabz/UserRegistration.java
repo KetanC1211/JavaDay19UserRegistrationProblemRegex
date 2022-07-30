2d20903d716310461d22b5fc09326dd90c699dbd
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-30 18:16:37 +0530

    Refactor UC8

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -33,18 +33,27 @@ public class UserRegistration {
 	}
 	
 	static void checkEmail() {
-		String emailCondition = "[abc]{3}.[a-z]{3}@bl.co.[a-z]{2}";
+		String emailCondition = "[abc]{3}([-+_])?([.a-zA-Z0-9]{2})?([A-Za-z0-9]{1,})?@[a-z0-9]{1,}.[a-z]{2,3}(.[a-z]{2,3})?";
 		Pattern pattern = Pattern.compile(emailCondition);
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the email ID");
-		String emailId = sc.nextLine();
-		Matcher email = pattern.matcher(emailId);
-		if(email.matches()) {
-			System.out.println("Valid Input");
-		}
-		else {
-			System.out.println("Invalid Input");
+		String validemailId [] = {"abc@yahoo.com","abc-100@yahoo.com","abc-100@yahoo.com",
+				"abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au",
+				"abc@1.com","abc@gmail.com.com","abc+100@gmail.com",
+				"abc","abc@.com.my","abc123@gmail.a","abc123@.com",
+				"abc123@.com.com",".abc@abc.com","abc()*@gmail.com",
+				"abc@%*.com","abc..2002@gmail.com","abc.@gmail.com",
+				"abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au",
+				"abc1..@gmail.com"};
+
+		for(int i=0;i<validemailId.length;i++) {
+			Matcher email = pattern.matcher(validemailId[i]);
+			if(email.matches()) {
+			
+			System.out.println(validemailId[i]+" Valid Input");
+			}
+			else {
+			System.out.println(validemailId[i]+" Invalid Input");
+			}
 		}
 	}
 
