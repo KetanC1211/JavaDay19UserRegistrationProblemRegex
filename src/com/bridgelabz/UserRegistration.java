c7338041f9d21c54af7a5765dfe2cb3df5bb76e0
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-30 11:33:27 +0530

    Refactor UC-7

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -65,7 +65,7 @@ public class UserRegistration {
 	}
 
 	static void checkPassword() {
-		String nameCondition = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8,}$";//atleast one uppercase
+		String nameCondition = "^(?=.*[@!#$%&*?])(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9@!#$%&*?]{8,}$";//atleast one [uppercase/numer/special char]
 		Pattern pattern = Pattern.compile(nameCondition);
 		
 		Scanner sc = new Scanner(System.in);
